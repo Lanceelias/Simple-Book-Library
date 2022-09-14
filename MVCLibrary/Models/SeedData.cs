c51e8205d2104b9b9b84a2672c995dcf4cf170b7
Lance <lance.obuyes@gmail.com>
2022-09-13 19:50:39 -0600

    Add project files.

---
+++ MVCLibrary/Models/SeedData.cs
@@ -0,0 +1,30 @@
+﻿using MVCLibrary.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCLibrary.Models
+{
+    public class SeedData
+    {
+        /// <summary>
+        /// Adding a data in a database that has a existing data.
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using(var context = new LibraryContext(
+                serviceProvider.GetRequiredService<DbContextOptions<LibraryContext>>()))
+            {
+                if(context.Book.Any())
+                {
+                    return;
+                }
+
+                context.Book.AddRange(
+                    new Book { Title = "Tiny C# Projects", CallNumber = "ABC 123", Author = "Ralph" },
+                    new Book { Title = "Tiny Android Projects", CallNumber = "ABD 123", Author = "James" }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
